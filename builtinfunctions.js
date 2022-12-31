718e026c9236f995a2f94c6927a45203f751c17d
nitishsehrawat1 <sehrawatnitish822@gmail.com>
2022-12-31 08:42:20 +0530

    typecasting

---
+++ builtinfunctions.js
@@ -0,0 +1,19 @@
+console.log('Hey Hi')
+
+const a = '10';
+const num1 = Number.parseInt(a);
+const b = '20';
+const num2 = Number.parseInt(b);
+
+console.log(num1 + num2);
+
+const isOkay = true;
+console.log(' ' + isOkay)
+
+const xyz = 123;
+
+const ab = (xyz).toString
+const bc = (isOkay).toString
+
+console.log(ab)
+console.log(bc)
