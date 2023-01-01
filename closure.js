f40b0b177bc018c0f7c39888d0cf19dad1dd848e
nitishsehrawat1 <sehrawatnitish822@gmail.com>
2023-01-01 17:35:38 +0530

    clousures

---
+++ closure.js
@@ -0,0 +1,8 @@
+function linearData(){ // outer function
+    const arr = [-3, -2, -1, 0, 1, 2, 3];
+
+    function plotdata() { // inner function
+        console.log(arr);
+    }
+    plotdata()
+}
