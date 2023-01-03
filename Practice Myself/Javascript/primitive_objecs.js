f85d18c96e3355389d9aaf1ed614597904e53eef
nitishsehrawat1 <sehrawatnitish822@gmail.com>
2023-01-03 15:58:51 +0530

    datatypes

---
+++ Practice Myself/Javascript/primitive_objecs.js
@@ -0,0 +1,9 @@
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt('567') + BigInt('3')
+let e = 'Nitish'
+let f = Symbol("I am a nice symbol")
+let g = undefined
+console.log(a, b, c, d, e, f)
+console.log(typeof g)
