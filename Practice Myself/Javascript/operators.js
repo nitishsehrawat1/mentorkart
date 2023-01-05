4eee472375b3319870254ac6e351320e88d7b777
nitishsehrawat1 <sehrawatnitish822@gmail.com>
2023-01-05 17:29:50 +0530

    Operators

---
+++ Practice Myself/Javascript/operators.js
@@ -0,0 +1,30 @@
+let a = 65;
+let b = 4;
+//  airthmetics operators
+// console.log(a**b)
+// console.log(a%b)
+console.log("a++ = " + a++) // 65
+console.log("++a = " + ++a) // 67
+console.log("--a = " + --a) //66
+console.log("a-- = " + a--)  //66
+console.log("a = " + a) // 65
+console.log("a-- = " + a--) //65
+
+//assignment operators 
+let c = 25;
+c += 5  //same as c = c + 5
+console.log("c = " + c) 
+ 
+c %= 4;
+console.log("c = " + c)
+
+// Comparison operators
+let d = 10;
+let e = 10;
+let f = "10"
+console.log("d == e   is " + d == e);
+console.log("d === e   is " + d === e);
+console.log("d == f   is " + d == f);
+console.log("d != e   is " + d != e);
+console.log("d === f   is " + d === f);
+console.log("d !== f   is " + d !== f);
