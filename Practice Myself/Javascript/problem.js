3be93ecf818138120150410ac11e79a81590131b
nitishsehrawat1 <sehrawatnitish822@gmail.com>
2023-01-11 12:06:34 +0530

    problem

---
+++ Practice Myself/Javascript/problem.js
@@ -0,0 +1,26 @@
+// const a = 21;
+// if (a<20 && a>10) console.log("You are eligible for this ");
+// else console.log("You are not eligible for this");
+
+// const age = 12;
+// switch (age) {
+//     case 12:
+//         console.log('Your age is 12')
+//         break
+//     case 13:
+//         console.log('Your age is 13')
+//         break
+//     case 14:
+//         console.log('Your age is 14')
+//         break
+//     case 14:
+//         console.log('Your age is 14')
+//         break
+//     default :
+//         console.log('Your age is not special')
+// }
+
+
+const num = 56;
+if (num % 2 == 0 || num % 3 == 0)
+    console.log('divisible')
