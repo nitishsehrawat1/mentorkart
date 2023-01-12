7da83088d4d24af427314eb7f1539a9076cda8aa
nitishsehrawat1 <sehrawatnitish822@gmail.com>
2023-01-12 15:24:51 +0530

    high order functions

--- Javascript/hof.js
+++ Javascript/hof.js
@@ -1,12 +1,32 @@
 function avgSum1() {
-    return function () {
-        console.log('This is another function')
-    }
+  // function is returning another function
+  let delta = 10;
+
+  // return function() {
+  //     return 10 * delta
+  // }
+
+  const ret = () => 10 * delta++;
+
+  delta = 322;
+
+  return ret;
 }
 
-function avgSum2() {
-
-    sm()
+function avgSum2(sm) {
+  // function is passed as parameter to other function
+  sm();
 }
 
-const avsm = () => {}
+const avg1 = avgSum1();
+console.log(avg1());
+console.log(avg1());
+console.log(avg1());
+console.log(avg1());
+
+const calcDelta = () => 0.5; // function is assigned to varible
+avgSum2(calcDelta);
+avgSum2(function () {
+  return 0.5;
+});
+avgSum2(() => 0.5);
