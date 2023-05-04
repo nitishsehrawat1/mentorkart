e9e8826b02b3e29dae9c2375df61a596c1094df6
nitishsehrawat1 <sehrawatnitish822@gmail.com>
2023-05-04 20:53:23 +0530

    problems on fuctions

---
+++ Practice Myself/Javascript/problems_on_functions.js
@@ -0,0 +1,18 @@
+let marks = {
+    nitish : 89,
+    harry : 90,
+    lovish : 9,
+    shubham : 2,
+}
+for (let i= 0; i<Object.keys(marks).length; i++){
+    console.log("The marks of " + Object.keys(marks)[i] + "are "  + marks[Object.keys(marks)[i]])
+}
+
+let cn = 4;
+ let c 
+ 
+ while(cn != c){
+     
+     c = prompt("Enter correct number")
+ } 
+ console.log("You have entered correct number")
