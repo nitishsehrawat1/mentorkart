dce62fb0b2266ce3b4f2de81cc6e17d281ef35f0
nitishsehrawat1 <sehrawatnitish822@gmail.com>
2023-05-20 14:52:10 +0530

    strings

---
+++ Practice Myself/Javascript/strings.js
@@ -0,0 +1,19 @@
+let name = "Nitish";
+console.log(name.length);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+console.log(name[5]);
+
+let boy1 = "Pramod";
+let boy2 = "Nikhil";
+let sentence = `${boy1}  is a friend of ${boy2} `
+console.log(sentence); 
+
+//Escape Sequence Characters
+let fruit = 'Banana\'s';
+console.log(fruit);
+
+console.log(fruit.toUpperCase())
---
+++ Practice Myself/Javascript/strings_methods.js
@@ -0,0 +1,22 @@
+let fruit = 'Banana\'s';
+console.log(fruit);
+
+console.log(fruit.toUpperCase());
+
+console.log(fruit.toLowerCase());
+
+let name = "nitish Bhai";
+
+console.log(name.slice(2,4));
+console.log(name.slice(2,));
+
+console.log(name.replace("n","N"))
+console.log(name.replace("Bhai","Bhau"))
+
+let friend  = " Nitishh";
+
+console.log(name.concat(" is a friend of" , friend ," Ok " , fruit) )
+
+let friend2 = "            Menna               ";
+console.log(friend2.trim())
+
